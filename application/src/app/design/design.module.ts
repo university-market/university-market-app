6946fc6dcd3a6d30644b160f861ab25d0f4dc42d
felipewesley <fe.wesleybasso@gmail.com>
2021-05-10 20:00:04 -0300

    use sidenav from material

--- application/src/app/design/design.module.ts
+++ application/src/app/design/design.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatSidenavModule } from '@angular/material/sidenav';
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatSidenavModule
   ],
-  declarations: []
+  declarations: [],
+  exports: [
+    MatSidenavModule
+  ]
 })
 export class DesignModule { }
