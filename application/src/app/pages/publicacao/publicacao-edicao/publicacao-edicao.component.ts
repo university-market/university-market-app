6873bd94cdec10afc56ad9a68262ae0bdaf5cd0e
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-05-31 00:25:20 -0300

    estrutura inicial form criacao e edicao

--- application/src/app/pages/publicacao/publicacao-edicao/publicacao-edicao.component.ts
+++ application/src/app/pages/publicacao/publicacao-edicao/publicacao-edicao.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { take, tap } from 'rxjs/operators';
+import { PublicacaoFormService } from '../services/publicacao-form.service';
+import { PublicacaoService } from '../services/publicacao.service';
+import { MASKS } from 'ng-brazil';
 @Component({
   selector: 'app-publicacao-edicao',
   templateUrl: './publicacao-edicao.component.html',
@@ -7,9 +12,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PublicacaoEdicaoComponent implements OnInit {
 
-  constructor() { }
+  public form: FormGroup;
+
+  public MASKS = MASKS;
+
+  public descricaoMaxLength = 256;
+
+  constructor(
+    private route: ActivatedRoute,
+    private formService: PublicacaoFormService,
+    public service: PublicacaoService,
+  ) { }
 
   ngOnInit() {
+
+    this.route.paramMap
+    .pipe(
+      take(1),
+    )
+    .subscribe((p: Params) => {
+      const publicacaoId = +p.get('publicacaoId');
+
+      this.service.init(publicacaoId);
+    });
+
+    this.form = this.formService.criarForm();
   }
 
 }
--- application/src/app/pages/publicacao/services/publicacao.service.ts
+++ application/src/app/pages/publicacao/services/publicacao.service.ts
@@ -1,10 +1,26 @@
 import { Injectable } from '@angular/core';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { BehaviorSubject } from 'rxjs';
 
 @UntilDestroy()
 @Injectable()
 export class PublicacaoService {
 
+  private _isEdicao = new BehaviorSubject<boolean>(false);
+  public isEdicao$ = this._isEdicao.asObservable();
+
+  private _publicacaoId = new BehaviorSubject<number>(0);
+  public publicacaoId$ = this._publicacaoId.asObservable();
+
   constructor() { }
+
+  public init(publicacaoId: number): void {
+
+    if (isNaN(publicacaoId))
+      return this._isEdicao.next(false);
+    
+    this._isEdicao.next(true);
+    this._publicacaoId.next(publicacaoId);
+  }
 
 }
