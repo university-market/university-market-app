ba8452c815268c9a02080dc6d15886343e1033b7
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-05-31 00:21:10 -0300

    valor constante nova publicacao

--- application/src/app/shared/const/default-consts.ts
+++ application/src/app/shared/const/default-consts.ts
@@ -8,3 +8,6 @@ export const DEFAULT_APP_TITLE = 'University Market';
 
 // Constante de rota definida para criação de novos recursos
 export const NEW_RESOURCE = 'new';
+
+// Constante de publicação com ID default para novas publicações (sem id)
+export const NEW_PUBLICACAO = 0;
