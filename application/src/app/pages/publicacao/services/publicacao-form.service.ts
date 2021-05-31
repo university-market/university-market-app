0e8b9727d0e3946b278aa0c10fcd19bc0fd2a2e3
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-05-31 00:21:34 -0300

    implementacao service criacao form

--- application/src/app/pages/publicacao/services/publicacao-form.service.ts
+++ application/src/app/pages/publicacao/services/publicacao-form.service.ts
@@ -1,11 +1,28 @@
 import { Injectable } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { NgBrazilValidators } from 'ng-brazil';
 
 @UntilDestroy()
 @Injectable()
 export class PublicacaoFormService {
 
   constructor(private fb: FormBuilder) { }
+
+  public criarForm(): FormGroup {
+
+    return this.fb.group({
+      titulo: this.fb.control(null),
+      tags: this.fb.array([]),
+      descricao: this.fb.control(null),
+      valor: this.fb.control(null, [NgBrazilValidators.currency]),
+      imagem: this.fb.control(null)
+    })
+  }
+
+  public criarTag(value?: string): FormControl {
+
+    return this.fb.control(value);
+  }
 
 }
