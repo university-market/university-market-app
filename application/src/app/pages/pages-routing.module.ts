556a02ec20177b874c426749e983d6e58dac5e7c
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-06-16 00:02:27 -0300

    correcao bug template sidenav active route

--- application/src/app/pages/pages-routing.module.ts
+++ application/src/app/pages/pages-routing.module.ts
@@ -4,8 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
     {
       path: '',
+      pathMatch: 'prefix',
+      redirectTo: 'home'
+    },
+    {
+      path: 'home',
       loadChildren: () => import("../pages/home/home.module").then(m => m.HomeModule)
-
     },
     {
       path: 'sales',
