bbc8bc0edc12bd54b77718ed56d958b43d091664
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-08-01 22:20:44 -0300

    remove old title service

--- application/src/app/app.module.ts
+++ application/src/app/app.module.ts
@@ -11,7 +11,6 @@ import { NgBrazil }from 'ng-brazil';
 import { DesignModule } from './design/design.module';
 import { PagesModule } from './pages/pages.module';
 import { SnackBarService } from './shared/services/snack-bar.service';
-import { TitleService } from './shared/services/title.service';
 import { SharedModule } from './shared/shared.module';
 import { TemplateModule } from './template/template.module';
 import { SelectedChipDirective } from './shared/directives/selected-chip.directive';
@@ -36,7 +35,6 @@ import { SelectedChipDirective } from './shared/directives/selected-chip.directi
     TemplateModule
   ],
   providers: [
-    TitleService,
     SnackBarService
   ],
   bootstrap: [AppComponent]
