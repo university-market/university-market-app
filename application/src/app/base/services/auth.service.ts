64de94ed3252c57c7fa563f4075bdcfc6e6ca46e
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-08-02 23:19:35 -0300

    updated base auth service

--- application/src/app/base/services/auth.service.ts
+++ application/src/app/base/services/auth.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  private _isAuthenticated = new BehaviorSubject<boolean>(false);
+  /**
+   * @property A boolean observable that said if user is authenticated or not
+   */
+  public isAuthenticated = this._isAuthenticated.asObservable();
 
   constructor() { }
 
