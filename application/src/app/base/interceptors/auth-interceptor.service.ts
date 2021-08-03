57b1055cf4fc7f43091f09cf2843ee940f703483
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-08-03 20:20:13 -0300

    http request error handling

--- application/src/app/base/interceptors/auth-interceptor.service.ts
+++ application/src/app/base/interceptors/auth-interceptor.service.ts
@@ -1,18 +1,26 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { NotificationService } from '../services/notification.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor() {}
-  
+  constructor(private _notification: NotificationService) {}
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     
     const request = req.clone();
 
     // Header for http requests, like authentication token, should be inserted here
 
-    return next.handle(request);
+    return next.handle(request)
+      .pipe(
+        catchError(error => {
+          this._notification.error(error.error.message, 0);
+          throw error;
+        })
+      );
   }
 }
