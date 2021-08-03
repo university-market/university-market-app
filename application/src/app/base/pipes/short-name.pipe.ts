a64eafa8751c9f92fce017236fab89d516a44080
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-08-02 22:18:26 -0300

    pipes module for project base

---
+++ application/src/app/base/pipes/short-name.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'shortName'
+})
+export class ShortNamePipe implements PipeTransform {
+
+  transform(name: string): string {
+
+    if (!name)
+      return null;
+
+    const whiteSpace = ' ';
+    const temp = name.trim().split(whiteSpace);
+
+    return temp[0].concat(whiteSpace).concat(temp[temp.length-1]).toString();
+  }
+
+}
--- application/src/app/base/university-market-base.module.ts
+++ application/src/app/base/university-market-base.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { DesignBaseModule } from './design/design-base.module';
 import { BaseConfirmDialogComponent } from './components/base-confirm-dialog/base-confirm-dialog.component';
 import { BaseQrCodeDialogComponent } from './components/base-qr-code-dialog/base-qr-code-dialog.component';
+import { UniversityMarketPipesModule } from './pipes/university-market-pipes.module';
 
 @NgModule({
   imports: [
     CommonModule,
     DesignBaseModule,
+    UniversityMarketPipesModule,
   ],
   declarations: [
     BaseConfirmDialogComponent,
@@ -17,6 +19,7 @@ import { BaseQrCodeDialogComponent } from './components/base-qr-code-dialog/base
   exports: [
     CommonModule,
     DesignBaseModule,
+    UniversityMarketPipesModule,
   ]
 })
 export class UniversityMarketBaseModule { }
