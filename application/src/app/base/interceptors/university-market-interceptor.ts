95b01859521cd764cced03c25f880bc8258a8b14
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-08-02 23:33:51 -0300

    snack bar erro in application core interceptor

--- application/src/app/base/interceptors/university-market-interceptor.ts
+++ application/src/app/base/interceptors/university-market-interceptor.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { catchError } from 'rxjs/operators';
+import { NotificationService } from '../services/notification.service';
 
 @Injectable()
 export class UniversityMarketInterceptor implements HttpInterceptor {
 
-    constructor() {}
+	constructor (private notification: NotificationService) {}
 
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+	intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-        console.log('req is', req);
+		console.log('req is', req);
 
-        return next.handle(req);
-    }
+		return next.handle(req)
+			.pipe(
+				catchError(error => {
+					this.notification.error(error.error.message, 0);
+					throw error;
+				})
+			);
+	}
 }
