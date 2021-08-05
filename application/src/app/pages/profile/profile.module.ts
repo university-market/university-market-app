3082a3b946cd59f0d636466713512e16a801b0ab
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-08-04 21:46:20 -0300

    base module in profile module

--- application/src/app/pages/profile/profile.module.ts
+++ application/src/app/pages/profile/profile.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { DesignModule } from 'src/app/design/design.module';
+import { UniversityMarketBaseModule } from 'src/app/base/university-market-base.module';
+
 import { ProfileComponent } from './profile.component';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileService } from './services/profile.service';
@@ -12,14 +13,12 @@ import { ProfileAccountDataComponent } from './components/profile-account-data/p
 import { ProfileContentComponent } from './profile-content/profile-content.component';
 import { ProfilePublicacoesHeaderComponent } from './components/profile-publicacoes/profile-publicacoes-header/profile-publicacoes-header.component';
 import { ProfilePublicacaoItemComponent } from './components/profile-publicacoes/profile-publicacao-item/profile-publicacao-item.component';
-import { NgBrazil } from 'ng-brazil';
 
 @NgModule({
   imports: [
     CommonModule,
+    UniversityMarketBaseModule,
     ProfileRoutingModule,
-    DesignModule,
-    NgBrazil,
   ],
   declarations: [
     ProfileComponent,
