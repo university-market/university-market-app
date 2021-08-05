661482c335d8bb44095ed7354443511fc78281e9
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-08-04 21:42:39 -0300

    update university market base module

--- application/src/app/base/university-market-base.module.ts
+++ application/src/app/base/university-market-base.module.ts
@@ -1,17 +1,29 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { AuthInterceptor } from './interceptors/auth-interceptor.service';
 
 import { DesignBaseModule } from './design/design-base.module';
+import { UniversityMarketPipesModule } from './pipes/university-market-pipes.module';
+
 import { BaseConfirmDialogComponent } from './components/base-confirm-dialog/base-confirm-dialog.component';
 import { BaseQrCodeDialogComponent } from './components/base-qr-code-dialog/base-qr-code-dialog.component';
-import { UniversityMarketPipesModule } from './pipes/university-market-pipes.module';
-import { UniversityMarketInterceptor } from './interceptors/university-market-interceptor';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthInterceptor } from './interceptors/auth-interceptor.service';
+
+import { TextMaskModule } from 'angular2-text-mask';
+import { NgBrazil } from 'ng-brazil';
 
 @NgModule({
   imports: [
-    CommonModule,
+    // Angular Forms
+    FormsModule,
+    ReactiveFormsModule,
+
+    // Mascaras de texto
+    TextMaskModule,
+    NgBrazil,
+
+    // Application Base
     DesignBaseModule,
     UniversityMarketPipesModule,
   ],
@@ -20,11 +32,21 @@ import { AuthInterceptor } from './interceptors/auth-interceptor.service';
     BaseQrCodeDialogComponent,
   ],
   exports: [
-    CommonModule,
+    // Angular Forms
+    FormsModule,
+    ReactiveFormsModule,
+
+    // Mascaras de texto
+    TextMaskModule,
+    NgBrazil,
+
+    // Application Base
     DesignBaseModule,
     UniversityMarketPipesModule,
+
   ],
   providers: [
+    FormBuilder,
     AuthInterceptor,
     {
       provide: HTTP_INTERCEPTORS,
