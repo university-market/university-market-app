09d8f7e510630b7e785691de3acf19d50cd73ea4
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-08-26 23:18:56 -0300

    estrutura inicial para controle de autenticacao

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 import { NotificationService } from 'src/app/base/services/notification.service';
 import { ForgotPasswordComponent } from '../forgot-password/forgot-password.component';
 import { LoginModel } from '../models/login.model';
@@ -15,6 +16,9 @@ import { LoginService } from '../services/login.service';
 export class LoginComponent implements OnInit {
 
   login: FormGroup;
+
+  private _triedLogin = new BehaviorSubject<boolean>(false);
+  public triedLogin$ = this._triedLogin.asObservable();
 
   constructor(
     private dialog: MatDialog,
@@ -31,22 +35,28 @@ export class LoginComponent implements OnInit {
   }
 
   // Função Responsável por realizar o login do usuário
-  doLogin(){
+  doLogin() {
+
     const model: LoginModel = {
       email: this.login.get('email')?.value,
       password: this.login.get('password')?.value,
     }
 
-    // Validação dos campos de login
-    if(model.email && model.password){
-      this.loginService.doLogin(model).subscribe(() =>{
+    if (!model.email || !model.password) {
+      
+      this.login.markAllAsTouched();
+      this._triedLogin.next(true);
+
+      this.notification.error("Todos os campos são obrigatórios");
+      return;
+    }
+    
+    this.loginService.doLogin(model)
+      .pipe()
+      .subscribe(() => {
         this.notification.success("Login Realizado com sucesso"),
         this.route.navigate(['/'])
-      })
-    }else{
-      this.notification.error("Favor preencher todos os campos!");
-      return
-    }
+      });
   }
 
   //Função responsável por abrir o modal de recuperação de senha
