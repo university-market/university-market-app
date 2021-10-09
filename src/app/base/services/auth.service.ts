da2ea5d7edd35798b2d4bf94090a88ab9e2cfd81
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-10-09 15:54:35 -0300

    método para obtenção de token de autenticação

--- src/app/base/services/auth.service.ts
+++ src/app/base/services/auth.service.ts
@@ -15,12 +15,12 @@ export class AuthService {
 
   private _isAuthenticated = new BehaviorSubject<boolean>(false);
   /**
-   * @property {boolean} isAuthenticated$ A boolean observable: user is authenticated or not
+   * @property User is authenticated or not
    */
   public isAuthenticated$ = this._isAuthenticated.asObservable();
 
   // Model resumida de session
-  private _authModel = new BehaviorSubject<AppSummarySession>(null);
+  // private _authModel = new BehaviorSubject<AppSummarySession>(null);
 
   /**
    * @property {UsuarioLogadoModel} user Model com dados do usuário logado no sistema
@@ -29,15 +29,44 @@ export class AuthService {
 
   constructor() { }
 
-  public persistSummarySession(model: AppSummarySession): void {
+  /**
+   * @method `persistSession` Persiste os dados da session recebidos da API
+   * @param {AppSummarySession} model Model de session recebida da API
+   * @returns `void`
+   */
+  public persistSession(model: AppSummarySession): void {
 
     // Define usuário como autenticado
     this._isAuthenticated.next(true);
 
     // Persiste a model de session em subject
-    this._authModel.next(model);
+    // this._authModel.next(model);
 
+    // Salvar token de autenticação da API
     this.persistAuthToken(model.token);
+  }
+
+  /**
+   * @method `getAuthToken()` Obtém o token de autenticação utilizado pela API
+   * @returns {string} `Token de autenticação`
+   */
+   public getAuthToken(): string {
+
+    return this._getAuthToken();
+  }
+
+  /**
+   * @method `setAsUnauthenticated()` Define o usuário como não autenticado
+   * @description Utilizado quando a API responde com status 403 (Não autenticado)
+   * @returns `void`
+   */
+   public setAsUnauthenticated(): void {
+
+    // Define usuário como não autenticado
+    this._isAuthenticated.next(false);
+
+    // Exclui o token de session previamente salvo
+    localStorage.removeItem(this.authTokenKey);
   }
 
   /**
@@ -45,7 +74,7 @@ export class AuthService {
    * @param {string} token Token de session recebida da API
    * @returns `void`
    */
-  public persistAuthToken(token: string): void {
+  private persistAuthToken(token: string): void {
 
     this._persistAuthToken(token);
   }
@@ -62,7 +91,10 @@ export class AuthService {
     localStorage.setItem(this.authTokenKey, token);
   }
 
+  private _getAuthToken(): string {
 
+    const token: string = localStorage.getItem(this.authTokenKey);
 
-
+    return token ?? null;
+  }
 }
