f1556643e16db7de7f00ae9482cc180f67bd7fea
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-10-17 18:18:25 -0300

    estrutura de steps para redefinição de senha

--- src/app/pages/auth/components/redefinir-senha/redefinir-senha.component.ts
+++ src/app/pages/auth/components/redefinir-senha/redefinir-senha.component.ts
@@ -1,15 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatStepper } from '@angular/material/stepper';
+import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
+import { NotificationService } from 'src/app/base/services/notification.service';
+import { RedefinicaoSenhaService } from '../../services/redefinicao-senha.service';
 
 @Component({
   selector: 'app-redefinir-senha',
   templateUrl: './redefinir-senha.component.html',
-  styleUrls: ['./redefinir-senha.component.scss']
+  styleUrls: ['./redefinir-senha.component.scss'],
+  providers: [RedefinicaoSenhaService]
 })
 export class RedefinirSenhaComponent implements OnInit {
 
-  constructor() { }
+  public formValidacaoEmail: FormGroup;
+  public emailCompleted: boolean = false;
 
-  ngOnInit() {
+  public formRedefinicaoSenha: FormGroup;
+
+  private _triedSave = new BehaviorSubject<boolean>(false);
+  public triedSave$ = this._triedSave.asObservable();
+
+  @ViewChild('stepper', {static: true}) stepper: MatStepper;
+
+  constructor(
+    private notification: NotificationService,
+    private route: Router
+  ) {
+    this.formValidacaoEmail = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email])
+    });
+
+    this.formRedefinicaoSenha = new FormGroup({
+      senha: new FormControl(null, Validators.required),
+      confirmacaoSenha: new FormControl(null, Validators.required)
+    });
+  }
+
+  ngOnInit() { }
+
+  onValidarEmail(): void {
+
+    const email = this.formValidacaoEmail.get('email')?.value;
+
+    if (this.formValidacaoEmail.invalid || !email) {
+
+      this._triedSave.next(true);
+      this.formValidacaoEmail.markAllAsTouched();
+
+      const msg = email ? 'Um e-mail válido deve ser informado' : 'Você deve informar seu e-mail';
+      this.notification.error(msg);
+
+      return;
+    }
+
+    // Request para validar email
+
+    if (this._triedSave.getValue())
+      this._triedSave.next(false);
+
+    this.emailCompleted = true;
+    this.stepper.next();
   }
 
 }
