118e0b56484e6094d4a566e52c68661cf9db5014
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-10-17 03:36:25 -0300

    validação do token recebido pelo guard de redefinicao

--- src/app/base/guards/redefinirsenha.guard.ts
+++ src/app/base/guards/redefinirsenha.guard.ts
@@ -1,14 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ActivatedRoute, ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable, of } from "rxjs";
+import { catchError, filter, map, take, tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { AuthService } from '../services/auth.service';
 import { NotificationService } from '../services/notification.service';
+
+const API_URL = environment.apiUrl + environment.auth;
 
 @Injectable()
 export class RedefinirSenhaGuard implements CanActivate {
 
   constructor(
     private authService: AuthService,
+    private http: HttpClient,
     private notification: NotificationService,
     private router: Router,
     private route: ActivatedRoute
@@ -19,20 +25,36 @@ export class RedefinirSenhaGuard implements CanActivate {
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
     Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
-    console.log(route);
+    const token = route.params['token'];
 
-    return true;
+    if (token == null) {
+
+      this.notification.error("Página não encontrada");
+
+      this.router.navigate(['/auth']);
+      return false;
+    }
+
+    return this.validarToken(token)
+      .pipe(
+        catchError(err => {
+
+          this.router.navigate(['/auth']);
+          throw err;
+        }),
+        filter((res: boolean) => res),
+        tap(() => this.notification.notify("A autenticidade da página foi verificada")),
+        // map(res => res as boolean)
+      );
   }
 
   // Private method
 
-  /**
-   * @method _alertNotAuthenticated() Alertar usuário que não está autenticado
-   */
-  private _alertNotAuthenticated(): void {
+  private validarToken(token: string): Observable<boolean> {
 
-    // Emissão de notificação
-    console.error('Você não está autenticado');
-    this.notification.warn('Você não está autenticado');
+    return this.http.get<boolean>(API_URL + `/estudante/recuperarsenha/${token}`)
+      .pipe(
+        take(1)
+      );
   }
 }
