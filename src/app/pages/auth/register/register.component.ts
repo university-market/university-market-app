3e3245c172ff902bf98889146153abfe6303bd26
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-11-12 00:21:17 -0300

    ajustes form cadastro - remocao campos nao utilizados

--- src/app/pages/auth/register/register.component.ts
+++ src/app/pages/auth/register/register.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { distinctUntilChanged, switchMap } from 'rxjs/operators';
-import { KeyValuePair } from 'src/app/base/data-types/key-value-pair';
+import { map } from 'rxjs/operators';
 import { NotificationService } from 'src/app/base/services/notification.service';
 import { PASSWORD_MINLENGHT } from 'src/app/core/static/password-data';
 import { RegistroInstituicaoEnsinoDialogComponent } from '../dialogs/registro-instituicao-ensino-dialog/registro-instituicao-ensino-dialog.component';
@@ -23,11 +22,7 @@ export class RegisterComponent implements OnInit {
   public passwordMinLenght: number = PASSWORD_MINLENGHT;
   public triedSave$ = new BehaviorSubject<boolean>(false);
 
-  // Dados necessários para formulário
-  public instituicaoList$: Observable<KeyValuePair<number, string>[]> = null;
-  public cursosList$: Observable<KeyValuePair<number, string>[]> = null
-
-  constructor(
+  constructor (
     private notification: NotificationService,
     private registerService: RegisterService,
     private router: Router,
@@ -37,36 +32,34 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
 
     this.form = this.registerService.form;
-    this.instituicaoList$ = this.registerService.instituicoes$;
-    this.cursosList$ = this.registerService.cursos$;
 
-    // Validacao de instituicao de ensino
+    // Validacao inicial cadastro - Instituicao de ensino e Curso
     this.dialog.open(RegistroInstituicaoEnsinoDialogComponent, {
       disableClose: true,
       width: '950px',
-
     })
       .afterClosed()
-      .pipe()
+      .pipe(
+        map((data: {instituicaoId: number|null, cursoId: number}) => ({
+          instituicaoId: data?.instituicaoId,
+          cursoId: data?.cursoId
+        }))
+      )
       .subscribe(data => {
 
-        console.log(data);
+        // Inicializacao dados institucionais do estudante
+        this.form.patchValue({
+          instituicao: data.instituicaoId,
+          curso: data.cursoId
+        });
       });
-
-
-    this.form.get('instituicao').valueChanges
-      .pipe(
-        distinctUntilChanged(),
-        switchMap(instituicaoId => this.registerService.buscarCursos(instituicaoId))
-      )
-      .subscribe();
   }
 
   // Função Responsável por realizar o cadatro do novo usuário
   public doRegister(): void {
     
     if (this.form.invalid) {
-      
+      console.log(this.form.getRawValue(), this.form);
       this.triedSave$.next(true);
       this.form.markAllAsTouched();
       this.notification.error('O formulário de cadastro deve ser preenchido corretamente', 3000);
@@ -87,25 +80,21 @@ export class RegisterComponent implements OnInit {
     const model: RegisterModel = {
       nome: this.form.get('nome').value,
       email: this.form.get('email').value,
-      ra: this.form.get('ra').value,
-      telefone: this.form.get('telefone').value,
       senha: this.form.get('senha').value,
       dataNascimento: this.form.get('dataNascimento').value,
       cursoId: this.form.get('curso').value,
       instituicaoId: this.form.get('instituicao').value
     };
 
-    console.log('model is', model);
-
     this.registerService.doRegister(model)
-    .subscribe(() => {
+      .subscribe(() => {
 
-      this.notification.success('Seu cadastro foi realizado com sucesso');
-      this.router.navigate(['/auth']);
-    }, (err) => {
+        this.notification.success('Seu cadastro foi realizado com sucesso');
+        this.router.navigate(['/auth']);
+      }, (err) => {
 
-      console.error(err);
-      this.notification.error('Não foi possível realizar seu cadastro');
-    });
+        console.error(err);
+        this.notification.error('Não foi possível realizar seu cadastro');
+      });
   }
 }
