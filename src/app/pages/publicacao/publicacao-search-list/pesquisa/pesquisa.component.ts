bc427b9b9c0c621a401d893d6aa7a42ef753b365
JV-Martins <jvk2001@gmail.com>
2021-11-22 20:16:11 -0300

    pesquisas

--- src/app/pages/publicacao/publicacao-search-list/pesquisa/pesquisa.component.ts
+++ src/app/pages/publicacao/publicacao-search-list/pesquisa/pesquisa.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NotificationService } from 'src/app/base/services/notification.service';
 
 @Component({
   selector: 'app-pesquisa',
@@ -7,9 +10,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PesquisaComponent implements OnInit {
 
-  constructor() { }
+  form : FormGroup
+
+  constructor(
+    private notification: NotificationService,
+    private route: Router
+  ) {
+    this.form = new FormGroup({
+      pesquisa : new FormControl(null,[Validators.required]),
+    });
+   }
 
   ngOnInit() {
+  }
+
+  validar(){
+    if(this.form.invalid){
+      this.notification.error('Favor informar o conteúdo da busca')
+      return false;
+    }
+
+    if (!this.form.get('pesquisa')?.value) {
+      
+      this.form.markAllAsTouched();
+
+      this.notification.error("Favor informar o conteúdo da busca");
+      return false;
+    }
+
+    return true;
+  }
+
+  pesquisar(){
+
+    if(!this.validar()){
+      return;
+    }
+    this.route.navigate(['/publicacao/busca'],{
+      queryParams:{
+        pesquisa : this.form.get('pesquisa')?.value 
+      }
+    }).then(() => {
+      window.location.reload();
+    });
+
   }
 
 }
