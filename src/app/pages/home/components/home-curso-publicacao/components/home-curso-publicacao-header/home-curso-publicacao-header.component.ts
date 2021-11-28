61971c701715ff01a1801a15b6ce7188e655158d
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-11-28 03:38:08 -0300

    melhoria layout header publicacoes do curso

--- src/app/pages/home/components/home-curso-publicacao/components/home-curso-publicacao-header/home-curso-publicacao-header.component.ts
+++ src/app/pages/home/components/home-curso-publicacao/components/home-curso-publicacao-header/home-curso-publicacao-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/base/services/auth.service';
 
 @Component({
   selector: 'app-home-curso-publicacao-header',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeCursoPublicacaoHeaderComponent implements OnInit {
 
-  constructor() { }
+  public curso: string = null;
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+
+    this.curso = this.authService.estudante.cursoNome;
   }
 
 }
