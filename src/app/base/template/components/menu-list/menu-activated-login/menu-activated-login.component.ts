4807795a10c85b37199d9fea31fed42fb6cb99fc
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-11-28 06:04:09 -0300

    ação de logou a partir da sidenav

--- src/app/base/template/components/menu-list/menu-activated-login/menu-activated-login.component.ts
+++ src/app/base/template/components/menu-list/menu-activated-login/menu-activated-login.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { delay, filter, switchMap } from 'rxjs/operators';
+
 import { AuthService } from 'src/app/base/services/auth.service';
+import { DialogService } from 'src/app/base/services/dialog.service';
+import { NotificationService } from 'src/app/base/services/notification.service';
 
 @Component({
   selector: 'app-menu-activated-login',
@@ -12,7 +17,14 @@ export class MenuActivatedLoginComponent implements OnInit {
   public primeiraLetraNome: string = null;
   public cursoNome: string = null;
 
-  constructor (private authService: AuthService) { }
+  public loading = true;
+
+  constructor (
+    private authService: AuthService,
+    private dialog: DialogService,
+    private notification: NotificationService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
 
@@ -21,6 +33,37 @@ export class MenuActivatedLoginComponent implements OnInit {
     this.cursoNome = this.authService.estudante?.cursoNome ?? 'Não possível obter o curso';
 
     this.primeiraLetraNome = this.primeiraLetraNome.toUpperCase();
+
+    setTimeout(() => this._stopLoading(), 2000);
   }
 
+  public logout(): void {
+
+    const message = `Deseja realmente sair do sistema? Você será imediatamente desconectado`;
+
+    this.loading = true;
+
+    this.dialog.openConfirmDialog(message, 'Desejo sair', 'Permanecer')
+      .pipe(
+        filter(res => {
+          if (!res) {
+            this._stopLoading();
+            return false;
+          }
+          return true;
+        }),
+        delay(1500),
+        switchMap(() => this.authService.logout())
+      )
+      .subscribe(() => {
+
+        this.notification.notify('Você foi desconectado!');
+        this.router.navigate(['/auth']);
+      });
+  }
+
+  private _stopLoading(): void {
+
+    this.loading = false;
+  }
 }
